bf79e06fd70728e64ec71336ba5859e598fa4e82
Hossein <hpourjafarabadi.dev@gmail.com>
2026-07-01 22:19:11 +0330

    Password hashing capability has been implemented

--- app/routers/users.py
+++ app/routers/users.py
@@ -4,16 +4,17 @@ from app.schemas.user import CreateUser
 from app.db.session import get_db
 from app.models.user import User
 from app.services.tokens import create_access_token, create_refresh_token
+from app.utils.hashing import hash_password
 
 router = APIRouter(prefix="/user", tags=["Users"])
 
 @router.post("/register")
-def create_user(data: CreateUser, db: Session = Depends(get_db)):
+def register_user(data: CreateUser, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.phone == data.phone).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Phone already exists")
     
-    new_user = User(name = data.name, phone = data.phone, password = data.password)
+    new_user = User(name = data.name, phone = data.phone, password = hash_password(data.password))
 
     db.add(new_user)
     db.commit()
