ae4c6195a6d2827cb4e4495e4e6d73891d200151
Hossein <hpourjafarabadi.dev@gmail.com>
2026-07-18 09:01:34 +0330

    Schema for creating siteinfo changed

--- app/schemas/site_info.py
+++ app/schemas/site_info.py
@@ -1,28 +1,28 @@
 from pydantic import BaseModel, Field
 from typing import List
-from app.schemas.base_site_info import BaseLocation, BasePhone, BaseLink, BaseWorkingHours, BaseSetting, BaseHero, BaseFooter, BaseAboutUs, BaseContactUs
+from app.schemas.base_site_info import CreateSlogans, CreateLocation, CreatePhone, CreateLink, CreateWorkingHours, CreateSetting, CreateHero, CreateFooter, CreateAboutUs, CreateContactUs
 
-class UpdateSiteInfo(BaseModel):
+class CreateSiteInfo(BaseModel):
     name: str | None = Field(None, min_length=1)
-    slogan: List[str] | None = None
+    slogans: List[CreateSlogans] | None = None
     logo: str | None = Field(None, pattern=r"^/media/uploads/[A-Za-z0-9_\-./]+\.(jpg|jpeg|png|webp)$")
 
     address: str | None = Field(None, min_length=1)
-    location: BaseLocation | None = None
+    location: CreateLocation | None = None
 
-    phones: List[BasePhone] | None = None
+    phones: List[CreatePhone] | None = None
 
-    links: List[BaseLink] | None = None
+    links: List[CreateLink] | None = None
 
-    working_hours: List[BaseWorkingHours] | None = None
+    working_hours: List[CreateWorkingHours] | None = None
 
     today_suggestions: List[str] | None = None
 
-    settings: List[BaseSetting] | None = None
+    settings: List[CreateSetting] | None = None
 
     table_reservation_time: int | None = Field(None, gt=0)
     
-    hero: BaseHero | None = None
-    footer: BaseFooter | None = None
-    about_us: BaseAboutUs | None = None
-    contact_us: BaseContactUs | None = None
+    hero: CreateHero | None = None
+    footer: CreateFooter | None = None
+    about_us: CreateAboutUs | None = None
+    contact_us: CreateContactUs | None = None
